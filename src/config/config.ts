f0b96883ceb631e07d84732214b0f89c403d85ab
Blacksox553103 <Snick6460@gmail.com>
2023-04-14 13:48:36 -0500

    updates from geel

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,43 +1,10 @@
 import { Unlockable } from "./types";
-
-/*********************************************************
-Replace the `unlockables` below with your own content. 
-Here are two examples:
-
-1️⃣ Unlock a Submarined file by owning one specific NFT.
-{ 
-  cid: "<YOUR_CID>", 
-  nftId: ["<YOUR_NFT_ID"]
-}
-
-2️⃣ Unlock a Submarined file by owning multiple NFTs.
-{
-  cid: "<YOUR_CID",
-  nftId: [
-    "<YOUR_FIRST_NFT_ID", "<YOUR_SECOND_NFT_ID", 
-  ]
-}
-*********************************************************/
-
 const unlockables: Unlockable[] = [
   {
-    cid: "bafybeiehgpaip4f7jafzf7imgannx3nnv3ubaiwp6ph56mlyzijpqxi45m", // HTML blog example
-    nftId: [
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' to unlock this.
-  },
-  {
-    cid: "bafybeihx5eacyxeydcpvudwxa242rnjhn67femy46gzas5d2djb24ti5mi", // MP4 video example
-    nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-    ], // You need to own 'GM #2' to unlock this.
-  },
-  {
-    cid: "bafybeihhx5v3saq3b7n55ub5q3atuw2udbqc5ictkv2ih7vd3hxptu22nu", // Web game example
-    nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' & 'GM #2' to unlock this.
+    cid: "bafkreighd4dcoibm53edplwfhrlzeazvt7nxu3dpculdco5kuqskpk6ll4", 
+nftId: [
+  "0x61b6f51013098cad1e7e4ed8da203e5a8d0e66975a911a369b155d1297dd28d4",
+    ], 
   },
 ];
 
