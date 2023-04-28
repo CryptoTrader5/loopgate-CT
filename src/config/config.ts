dcd3f40b5931e72c3546f4dbf951a3f0a82c0459
Blacksox553103 <Snick6460@gmail.com>
2023-04-28 17:29:53 -0500

    Test with 4 boxes

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,31 @@
 import { Unlockable } from "./types";
 const unlockables: Unlockable[] = [
+  // Gaming box 1
   {
     cid: "bafkreighd4dcoibm53edplwfhrlzeazvt7nxu3dpculdco5kuqskpk6ll4", 
 nftId: [
-  "0x61b6f51013098cad1e7e4ed8da203e5a8d0e66975a911a369b155d1297dd28d4",
+  "0xd85525bd5125c69808183ef53c236212b4501e9e83e4193a6d5bed3da405d01e",
+    ], 
+  },
+  // Gaming box 2
+  {
+    cid: "bafkreihqprpi6ywh4fgodytqt6wmwcnvnuqtsflnh46tal3sizw7bm2n4u", 
+nftId: [
+  "0x7da1fe28b8cb7fa601940a6f4e58046f9837d702e26e81eed3599403eebedb02",
+    ], 
+  },
+  // Stirling Grooming 1
+  {
+    cid: "bafkreif3pp4knwvnjbfmrh6pvkmvu4pclnlvpwiu6glqvikyu5pdk33nia", 
+nftId: [
+  "0x248af0d1158d7b8fa90064030f3d3eda2bf9705b518959e923a146e1553308a5",
+    ], 
+  },
+  // Stirling Grooming 2
+  {
+    cid: "bafkreihzoq73crl5fvruksuqewymtlwqklqthfphdhfe2tmsa2l2jxdrey", 
+nftId: [
+  "0x978900cc9553388fef5602293f1df4ca4ec43370fa7e2c308041c01fb3aa3c36",
     ], 
   },
 ];
